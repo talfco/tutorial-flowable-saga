d1d664b5f50b36e87a92ada8ddd76da81985af83
felix <felix@cloudburo.net>
2019-04-14 23:26:29 +0200

    Full working tutorial

--- src/main/java/net/cloudburo/DemoApplication.java
+++ src/main/java/net/cloudburo/DemoApplication.java
@@ -1,20 +1,12 @@
 package net.cloudburo;
 
-import org.flowable.engine.RepositoryService;
-import org.flowable.engine.RuntimeService;
-import org.flowable.engine.TaskService;
-import org.flowable.engine.repository.ProcessDefinition;
-import org.flowable.engine.repository.ProcessDefinitionQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ApplicationContext;
 
-import java.util.List;
-
-//@ComponentScan(basePackages = "net.cloudburo")
 @SpringBootApplication
 public class DemoApplication {
 
@@ -24,31 +16,4 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner init(final RepositoryService repositoryService,
-                                  final RuntimeService runtimeService,
-                                  final TaskService taskService) {
-
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... strings) throws Exception {
-                ProcessDefinitionQuery query = repositoryService.createProcessDefinitionQuery();
-                logger.info("Number of process definitions : " + query.count());
-                List<ProcessDefinition> definitions = query.list();
-                for (ProcessDefinition definition : definitions) {
-                    logger.info("Loaded Process Definitions: "+definition.getKey());
-                }
-
-                // Execute the Intro Process
-                runtimeService.startProcessInstanceByKey("introProcess");
-                //System.out.println("Number of tasks after process start: "
-                //        + taskService.createTaskQuery().count());
-
-                // Execute the Saga1 Process
-                runtimeService.startProcessInstanceByKey("saga1Process");
-                //System.out.println("Number of tasks after process start: "
-                //        + taskService.createTaskQuery().count());
-            }
-        };
-    }
 }
--- src/main/java/net/cloudburo/camel/saga1/Saga1ProcessRoutes.java
+++ src/main/java/net/cloudburo/camel/saga1/Saga1ProcessRoutes.java
@@ -1,7 +1,9 @@
 package net.cloudburo.camel.saga1;
 
+import net.cloudburo.app.EventMessage;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,13 +15,22 @@ public class Saga1ProcessRoutes extends RouteBuilder {
 
     @Override
     public void configure()  {
-
+        // Flow triggered from Flowable Task
         from("flowable:saga1Process:camelAsyncTask")
                 .to("seda:continueAsync")
-                .log(LoggingLevel.INFO, logger, "External System Processing...")
+                .log(LoggingLevel.INFO, logger, "Internal Flowable Camel Route: ${propertyInput}")
                 .transform().simple("Processed: ${property.input}, { Result: OK }");
-
-        from("seda:continueAsync")
+                //.to("flowable:saga1Process:receiveAsyncTask");
+        // Flow triggered from a rabbit mq delivery
+        from("rabbitmq:spring-boot-exchange1?exchangeType=topic&routingKey=cloudburo.demo")
+                .log(LoggingLevel.INFO, logger, "Rabbit MQ Message Received: ${body}")
+                .unmarshal().json(JsonLibrary.Jackson, EventMessage.class)
+                .log(LoggingLevel.INFO, logger, "Unmarshalled Response: ${body.id}")
+                .process(exchange -> {
+                      EventMessage msg = exchange.getIn().getBody(EventMessage.class);
+                      exchange.getIn().setBody(msg.getMessage());
+                      exchange.setProperty("PROCESS_ID_PROPERTY",msg.getId());
+                })
                 .to("flowable:saga1Process:receiveAsyncTask");
     }
 }
